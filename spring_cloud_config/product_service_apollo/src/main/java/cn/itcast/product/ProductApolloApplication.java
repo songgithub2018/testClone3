a10e2a5de527c9914daaa59ef4ac575a544babcb
lins-c <songlam@yeah.net>
2022-01-08 21:10:25 +0800

    首次提交

---
+++ spring_cloud_config/product_service_apollo/src/main/java/cn/itcast/product/ProductApolloApplication.java
@@ -0,0 +1,12 @@
+package cn.itcast.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductApolloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductApolloApplication.class,args);
+    }
+}
